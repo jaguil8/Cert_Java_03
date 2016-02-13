b7d6d7792398e0bec972847c4ed3c2760b45a308
Jesús Aguilera <jaguil@hotmail.com>
2016-02-13 16:50:52 -0600

    Mostrar Equipos de red, proyecto de tarea Antes de editar y eliminar

--- Cert03_Tarea02/src/main/webapp/js/indexcontroller.js
+++ Cert03_Tarea02/src/main/webapp/js/indexcontroller.js
@@ -3,11 +3,32 @@ var AppEquiposred = angular.module('AppEquiposred',[]);
 
 AppEquiposred.controller('mostrarEquiposred', function($scope, $http){
     
-    $scope.saludo = "Hola mundo";
     
-    //Llamada a get con callback
-    $http.get('equiposred')
-            .success(function(data, status){
-                $scope.Equiposdered = data;
-            });
+    
+    $scope.cargarEquiposred = function(){
+        //Llamada a get con callback
+        $http.get('equiposred')
+                .success(function(data, status){
+                    $scope.Equiposdered = data;
+                });
+    };
+            
+    $scope.agregarEquipodered = function(){
+        $http.post('equiposred', $scope.nuevoEquipored)
+                .success(function(data, status){
+                    //$scope.Equiposdered.push($scope.nuevoEquipored);
+                    $scope.nuevoEquipored = {};
+                    $scope.cargarEquiposred();
+                    $scope.estaAgregando = false;
+                })
+                .error(function(data, status){
+                    console.log('ERROR:', data);
+                    $scope.estaAgregando = false;
+                });
+    };
+    
+    $scope.saludo = "Hola mundo";
+    $scope.nuevoEquipored = {};
+    $scope.cargarEquiposred();
+    
 });
